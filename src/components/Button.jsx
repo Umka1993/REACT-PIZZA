8e507b444090bc87e807e5d3e9ff29d409603d68
Ivan <vanish475@gmail.com>
2021-11-02 16:01:48 +0200

    created component Categories.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import classNames from "classnames";
 
-const Button=(props)=> {
-    console.log(this)
+const Button=({onClick, className, outline, children})=> {
     return(
         <div>
-            <button className={classNames('button', {
-                'button--outline': props.outline
-            })}>{props.children}</button>
+            <button
+                onClick={onClick}
+                className={classNames('button', className, {
+                'button--outline': outline
+            })}>{children}</button>
+
+
         </div>
     )
 }
